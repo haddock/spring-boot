9d5027d8ac1a88647cd29180453a7d1489747042
Fredrik Bergljung <bergljung@gmail.com>
2015-12-11 14:29:52 +0100

    fixed untracked files

--- src/main/java/hello/dao/MovieRepository.java
+++ src/main/java/hello/dao/MovieRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
  * Created by fredrik.bergljung on 2015-12-10.
  */
 public interface MovieRepository extends ElasticsearchRepository<Movie, Long> {
-    public List<Movie> findByName(String name);
-    public List<Movie> findByRatingBetween(Double beginning, Double end);
+    List<Movie> findByName(String name);
+    List<Movie> findByRatingBetween(Double beginning, Double end);
 }
